c12bed01d41e4165dbdcb62f220881252d7ba8d9
jheave <jimenezjheave123@gmail.com>
2022-07-10 15:06:39 +0800

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,20 @@ import Todo from './models/Todo';
 const App: React.FC = () => {
 	const [todo, setTodo] = useState<string>("");
 	const [todos, setTodos] = useState<Todo[]>([]);
-	
+
+	const handleAdd = (e: React.FormEvent) => {
+		e.preventDefault();
+		todo && setTodos(
+			[...todos, {id: Date.now(), title: todo, isCompleted: false}]
+		);
+		setTodo("")
+	}
+	console.log(todos);
+
 	return (
 		<div className='App'>
 			<span className='heading'>Todo</span>
-			<InputField todo={todo} setTodo={setTodo}/> 
+			<InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
 		</div>
 	)
 }
